322a5494a883c3d893b45ff9b17c7fae43cebd5c
hanishkumar <hanishk616@gmail.com>
2019-10-31 00:26:51 +0530

    Added Readme

--- app/src/main/java/com/xyz/androideatit/Service/NotificationHelper.java
+++ app/src/main/java/com/xyz/androideatit/Service/NotificationHelper.java
@@ -56,7 +56,7 @@ public class NotificationHelper extends ContextWrapper {
                 .setAutoCancel(true)
                 .setWhen(System.currentTimeMillis())
                 .setTicker("Hanish")
-                .setContentText("Your Order is Updated to " + Common.convertCodeToStatus(request.getStatus()))
+                .setContentText("Your Order " + key + "is " + Common.convertCodeToStatus(request.getStatus()))
                 .setContentTitle("Eat It")
                 .setContentIntent(contentIntent)
                 .setSmallIcon(R.mipmap.ic_launcher);
